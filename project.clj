e624c7028af613313eae63f60fe9451c0735f6c8
David Rouchy <drouchy@gmail.com>
2015-03-21 14:08:21 +0000

    version 1.0.0 with the doc

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject basic-service/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject basic-service/lein-template "1.0.0"
+  :description "Build a compojure-api project with few additionals libraries"
+  :url "https://github.com/drouchy/basic-service-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
